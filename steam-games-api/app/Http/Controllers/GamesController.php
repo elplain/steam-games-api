68e689c9fbe33d515b7dbe57c1b23eaff06b0c14
elplain322 <zinyachok@gmail.com>
2026-04-28 00:38:27 +0100

    1

--- steam-games-api/app/Http/Controllers/GamesController.php
+++ steam-games-api/app/Http/Controllers/GamesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Game;
+use App\Http\Resources\GamesResource;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 
@@ -12,32 +13,37 @@ class GamesController extends Controller
      * Display a listing of the resource.
      */
     public function index()
-    {
-        return Game::with('genres')->get();
+  {
+        $games = Game::with('genres')->get();
+
+        return GamesResource::collection($games);
     }
+
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        $data = $request->validate([
+            $data = $request->validate([
             'title' => ['required', 'string', 'max:255', 'unique:games,title'],
             'description' => ['nullable', 'string'],
             'genres' => ['array'],
             'genres.*' => ['integer', 'exists:genres,id'],
+
         ]);
 
-        $game = Game::create([
+            $game = Game::create([
             'title' => $data['title'],
             'description' => $data['description'] ?? null,
+
         ]);
 
         if (!empty($data['genres'])) {
             $game->genres()->attach($data['genres']);
         }
 
-        return $game->load('genres');
+        return new GamesResource($game->load('genres'));
     }
 
     /**
@@ -45,13 +51,13 @@ class GamesController extends Controller
      */
     public function show(Game $game)
     {
-        return $game->load('genres');
+        return new GamesResource($game->load('genres'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Game $game)
+       public function update(Request $request, Game $game)
     {
         $data = $request->validate([
             'title' => [
@@ -74,17 +80,8 @@ class GamesController extends Controller
             $game->genres()->sync($data['genres']);
         }
 
-        return $game->load('genres');
+        return new GamesResource($game->load('genres'));
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Game $game)
-    {
-        $game->delete();
-
-        return response()->noContent();
-    }
 }
 
