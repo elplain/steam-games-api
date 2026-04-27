bbffe07768414b3bda460e18d58af528b358eee4
AKovalenko <Kovalenko-A@ulster.ac.uk>
2026-04-27 22:27:49 +0100

    apichange

--- steam-games-api/routes/api.php
+++ steam-games-api/routes/api.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Appp\HttpControllers\GameController;
+use App\Http\Controllers\GamesController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route :: apiResource ( ' game s' ,   Games Controller :: class );
+Route :: apiResource ( 'games',GamesController::class );
